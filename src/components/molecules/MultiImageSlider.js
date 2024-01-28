feba7d1b6574ad37df10a20ac77297cc85872845
suraj sangale <surajdsangale@gmail.com>
2024-01-28 19:40:39 +0530

    optimize slider code

---
+++ src/components/molecules/MultiImageSlider.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import "./TopDeals.css";
+import Heading from "./Heading";
+
+const MultiImageSlider = (props) => {
+    const { desktopItems, mobileItems, tabletItems, title, ImgArr } = props;
+    const responsive = {
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: desktopItems,
+            slidesToSlide: 4
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 768 },
+            items: tabletItems,
+            slidesToSlide: 3
+        },
+        mobile: {
+            breakpoint: { max: 767, min: 464 },
+            items: mobileItems,
+            slidesToSlide: 1
+        }
+    };
+    return (
+        <div className="parent">
+            <Heading title={title} />
+            <Carousel
+                responsive={responsive}
+                autoPlay={true}
+                swipeable={true}
+                draggable={true}
+                showDots={true}
+                infinite={true}
+                partialVisible={false}
+                dotListClass="custom-dot-list-style"
+            >
+                {ImgArr?.map((item, index) => {
+                    return (
+                        <div className="slider" key={index}>
+                            <div className="image-container">
+                                <img src={item?.url} alt="movie" className="hover-image" />
+                            </div>
+                        </div>
+                    );
+                })}
+            </Carousel>
+        </div>
+    );
+}
+
+export default MultiImageSlider
--- src/components/molecules/TopDeals.js
+++ src/components/molecules/TopDeals.js
@@ -1,51 +1,18 @@
 import React from "react";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-import "./TopDeals.css";
 import { topDealsImages } from "../Data/Data";
-import Heading from "./Heading";
-const responsive = {
-    desktop: {
-        breakpoint: { max: 3000, min: 1024 },
-        items: 6,
-        slidesToSlide: 4
-    },
-    tablet: {
-        breakpoint: { max: 1024, min: 768 },
-        items: 5,
-        slidesToSlide: 3
-    },
-    mobile: {
-        breakpoint: { max: 767, min: 464 },
-        items: 4,
-        slidesToSlide: 1
-    }
-};
+import MultiImageSlider from "./MultiImageSlider";
+
 const TopDeals = () => {
     return (
-        <div className="parent">
-            <Heading title={'Top Deals'} />
-            <Carousel
-                responsive={responsive}
-                autoPlay={true}
-                swipeable={true}
-                draggable={true}
-                showDots={true}
-                infinite={true}
-                partialVisible={false}
-                dotListClass="custom-dot-list-style"
-            >
-                {topDealsImages.map((imageUrl, index) => {
-                    return (
-                        <div className="slider" key={index}>
-                            <div className="image-container">
-                                <img src={imageUrl.url} alt="movie" className="hover-image" />
-                            </div>
-                        </div>
-                    );
-                })}
-            </Carousel>
-        </div>
-    );
+        <>
+            <MultiImageSlider
+                title={'Top Deals'}
+                desktopItems={6}
+                tabletItems={5}
+                mobileItems={4}
+                ImgArr={topDealsImages}
+            />
+        </>
+    )
 };
 export default TopDeals;
