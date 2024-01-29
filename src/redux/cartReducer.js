fc1aeafa2713c9dbf1ee6c0af6ffa9f7ffa9b183
suraj sangale <surajdsangale@gmail.com>
2024-01-29 12:46:43 +0530

    chnages

--- src/redux/cartReducer.js
+++ src/redux/cartReducer.js
@@ -14,7 +14,6 @@ const cartReducer = (state = initialState, action) => {
       const indexToRemove = state.cartItems.findIndex((item) => item.id === id);
 
       if (indexToRemove !== -1) {
-        // Remove the first matching item by creating a new array
         const updatedCartItems = [
           ...state.cartItems.slice(0, indexToRemove),
           ...state.cartItems.slice(indexToRemove + 1),
